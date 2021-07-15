4f0a6c99aa6e6603757b6e5f5932a7d18c215fa2
ctyever <ctyever@naver.com>
2021-07-15 19:51:51 +0900

    EarlyStopping, 이진분류, 다중분류

---
+++ keras/keras23_EarlyStopping.py
@@ -0,0 +1,97 @@
+# 과제3
+from numpy.testing._private.nosetester import _numpy_tester
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+from sklearn import datasets
+from sklearn.datasets import load_boston
+import numpy as np
+
+#1. 데이터
+datasets = load_boston()
+x = datasets.data
+y = datasets.target
+
+# print(np.min(x), np.max(x)) # 0.0 711.0
+
+#데이터 전처리 (0~1사이로 바꿈) 노말라이제이션, 정규화
+# x = x/711.
+# x = x/np.max(x)
+# x = (x - np.min(x)) / (np.max(x) - np.min(x))
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, 
+        train_size=0.7, shuffle=True, random_state=66)
+
+        
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+# scaler = MinMaxScaler()
+scaler = StandardScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
+# print(x.shape) # (506, 13)
+# print(y.shape) # (506,)
+
+# print(datasets.feature_names)
+# print(datasets.DESCR) # feature 자세히 나옴
+# print(datasets['filename'])
+
+#2. 모델구성
+model = Sequential()
+model.add(Dense(5, input_dim=13))
+model.add(Dense(100))
+model.add(Dense(3))
+model.add(Dense(100))
+model.add(Dense(3))
+model.add(Dense(1))
+
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+
+from tensorflow.keras.callbacks import EarlyStopping
+es = EarlyStopping(monitor='val_loss', patience=5, mode='min', verbose=1)
+
+
+hist = model.fit(x_train, y_train, epochs=100, batch_size=8, callbacks=[es], validation_split=0.2)
+
+
+print(hist.history.keys())
+print(hist.history['loss'])
+print(hist.history['val_loss'])
+
+import matplotlib.pyplot as plt
+
+import matplotlib
+matplotlib.font_manager._rebuild()
+
+plt.rc('font', family='NanumGothic')
+print(plt.rcParams['font.family'])
+
+
+plt.plot(hist.history['loss'] ) # x: epoch / y: hist.history['loss']
+plt.plot(hist.history['val_loss'] )
+
+plt.title("산점도")
+plt.xlabel('epochs')
+plt.ylabel('loss, val_loss')
+plt.legend(['train loss', 'val loss']) # 범례
+plt.show()
+
+# #4. 평가, 예측
+# loss = model.evaluate(x_test, y_test)
+# print('loss : ', loss)
+# y_predict = model.predict(x_test)
+# # # print('예측값 : ', y_predict)
+
+# #5. r2 구하기
+# r2 = r2_score(y_test, y_predict)
+
+# print("r2스코어 : ", r2)
+
+# # epochs=1000, batch_size=1, loss :  17.291133880615234 r2스코어 :  0.7907075553752195
+# # 1000, 1, layer 변경 , loss :  17.116796493530273, r2스코어 :  0.7928177634040774
+# # 10000, 1, loss :  16.508121490478516, r2스코어 :  0.8001851831896087
---
+++ keras/keras26_wine.py
@@ -0,0 +1,79 @@
+import numpy as np
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from sklearn.datasets import load_wine
+
+# 완성하시오!!
+# acc 0.8 이상 만들것
+
+# 1. 데이터 구성
+datasets = load_wine()
+
+# print(datasets.DESCR)
+# print(datasets.feature_names) # ['alcohol', 'malic_acid', 'ash', 'alcalinity_of_ash', 'magnesium', 'total_phenols', 'flavanoids', 'nonflavanoid_phenols', 'proanthocyanins', 'color_intensity', 'hue', 'od280/od315_of_diluted_wines', 'proline']
+
+x = datasets.data
+y = datasets.target
+
+# print(x.shape, y.shape) # (178, 13) (178,)
+# print(y)
+'''
+[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+ 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+ 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+ 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2]
+'''
+
+from tensorflow.keras.utils import to_categorical
+y = to_categorical(y)
+# print(y[:5])
+# print(y.shape) # (178, 3)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, 
+        train_size=0.7, shuffle=True, random_state=9)
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, MaxAbsScaler, RobustScaler, QuantileTransformer, PowerTransformer
+scaler = MinMaxScaler()
+# scaler = StandardScaler()
+# scaler = MaxAbsScaler()
+# scaler = RobustScaler()
+# scaler = QuantileTransformer()
+# scaler = PowerTransformer()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
+# 2. 모델 구성
+input1 = Input(shape=(13,))
+dense1 = Dense(128, activation='relu', name='dense1')(input1)
+dense2 = Dense(64, activation='relu', name='dense2')(dense1)
+dense3 = Dense(64, activation='relu', name='dense3')(dense2)
+dense4 = Dense(64, activation='relu', name='dense4')(dense3)
+dense5 = Dense(32, activation='relu', name='dense5')(dense4)
+output1 = Dense(3, activation='softmax', name='output1')(dense5)
+
+model = Model(inputs= input1, outputs=output1)
+
+#3. 컴파일, 훈련
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']) # 다중분류에서 loss 는 categorical_crossentropy
+
+from tensorflow.keras.callbacks import EarlyStopping
+es = EarlyStopping(monitor='val_loss', patience=5, mode='min', verbose=1)
+
+model.fit(x_train, y_train, epochs=100, batch_size=8, callbacks=[es], validation_split=0.3, verbose=2)
+
+#4. 평가, 예측
+loss = model.evaluate(x_test, y_test)
+print('loss : ', loss[0])
+print('accuracy : ', loss[1])
+
+# loss :  0.06762813031673431
+# accuracy :  0.9629629850387573
+
+# 예측
+# print(y_test[:5])
+# y_predict = model.predict(x_test[:5])
+# print(y_predict)
